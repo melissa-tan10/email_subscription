4e5f403b5229b7f8dbcb5f5378e290e043c1b4c9
melissa-tan10 <69818577+melissa-tan10@users.noreply.github.com>
2020-09-16 23:35:55 -0400

    Add files via upload

---
+++ subscription.js
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<title>Email Subscription</title>
+</head>
+<body>
+<form>
+    <div>
+        <div>
+            <input type="email" class="form" placeholder="Enter your email">
+        </div>
+        <div>
+            <button type="button" class="form full">Subscribe</button>
+        </div>
+    </div>
+<form>
+</body>
+</html>
